4a9e6e676eb12b8e65563d3eabfbdf75d4bf35f6
Duzaoex <eduardoneriscarneiro@gmail.com>
2023-10-26 00:50:33 -0300

    responsive iniciando

--- src/Pages/Header/Header.tsx
+++ src/Pages/Header/Header.tsx
@@ -3,12 +3,10 @@ import logo from '../../util/images/logoTransparent.png';
 
 function Header() {
     return (
-        <>
-            <header className={style.header}>
-                <h1>Portfólio</h1>
-                <img className={style.logo} src={logo} alt="logo"></img>
-            </header>
-        </>
+        <header className={style.header}>
+            <h1 className={style.title}>Portfólio</h1>
+            <img className={style.logo} src={logo} alt="logo"></img>
+        </header>
     )
 }
 
