4b65114251efbae23ff494fad0d8ba972e83ea24
Duzaoex <eduardoneriscarneiro@gmail.com>
2023-11-08 13:18:27 -0300

    add projetos

--- src/Componentes/ListaDosProjetos/ListaDeProjetos.tsx
+++ src/Componentes/ListaDosProjetos/ListaDeProjetos.tsx
@@ -1,13 +1,26 @@
 export const listaDosProjetos = [
+
+    {
+        titulo: 'Lessons learned',
+        descricao: 'Fundamentos Desenvolvimento Web - HTML E CSS ',
+        link: 'https://github.com/Duzaoex/Projetos/tree/main/Projeto%20Lessons%20learned',
+    },
+
+    {
+        titulo: 'Playground Functions',
+        descricao: 'Fundamentos Desenvolvimento Web -  Aprendento as funções',
+        link: 'https://github.com/Duzaoex/Projetos/tree/main/Projeto%20Playground%20Functions',
+    },
+
     {
         titulo: 'iChoveu',
-        descricao: 'Projeto que usa uma API para verificar a previsão do tempo',
+        descricao: 'FRONTEND - Projeto que usa uma API para verificar a previsão do tempo',
         link: 'https://github.com/Duzaoex/Projetos/tree/main/Projeto%20iChoveu',
     },
 
     {
         titulo: 'Password Manager',
-        descricao: 'Aplicação em React para gerenciar senha',
+        descricao: 'FRONTEND - Aplicação em React para gerenciar senha',
         link: 'https://github.com/Duzaoex/Projetos/tree/main/Projeto%20Password%20Manager',
     },
 ];
